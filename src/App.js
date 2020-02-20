1d41b73621fd164bddf0383a69594a4692bf6120
Jared Brown <jaredbrown1@hotmail.com>
2020-02-19 23:53:06 -0800

    section 1 complete

---
+++ src/App.js
@@ -0,0 +1,30 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", { id: "something-important" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Khaleesi",
+      animal: "Dog",
+      breed: "Doberman"
+    }),
+    React.createElement(Pet, {
+      name: "Monty",
+      animal: "Snake",
+      breed: "Ball Python"
+    }),
+    React.createElement(Pet, {
+      name: "Holly",
+      animal: "Dog",
+      breed: "Dachsund"
+    })
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
